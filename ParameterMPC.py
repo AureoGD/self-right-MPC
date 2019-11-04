b84d55724d8feda93bf72526036cc0ebb751c12a
Aureo <aureogd@gmail.com>
2019-11-04 10:32:15 -0300

    Changed the initialization of the class. Created the function to update the constraints and gains matrices.

--- ParameterMPC.py
+++ ParameterMPC.py
@@ -30,8 +30,8 @@ and
     Phi = [CA CA^2 ... CA^N-1]
 The dimensions of H depends of the M and N.
 
-The system want to respect the friction cone, so, is established a constraint:
-    (C2*C1†*Wg)*[qb[k/k] ... qb[k+M/k]' <= [0 ... 0]
+The system want to respect the friction cone, so, it is established a constraint:
+    (C2*C1†*Wg)*[qb[k/k] ... qb[k+M/k]' <= [0 ... 0]'
 This constraint depends of the pose of the robot.
 The joints accelerations, speed and positions are also constrained:
     qdd_min <= qdd[k/k] <= qdd_max
@@ -50,64 +50,42 @@ from scipy.linalg import block_diag
 from CentroidalMomentumMatrix import CentroidalMomentumMatrix
 from updateKin import updateKin
 
+DEFAULT_FRIC_COEF = 1
+
 
 class ParametersMPC:
 
-    def __init__(self, N, M, ts, q_max, q_min, qd_max, qd_min, qdd_max, qdd_min, Q, R, fric_coef):
+    def __init__(self, N, M, R, Q, ts):
         """General variable"""
         self.N = N  # predict horizon
         self.M = M  # control horizon
         self.ts = ts  # sample time
+
+        """Initialization of external class"""
+        self.cmm = CentroidalMomentumMatrix()  # Centroidal Momentum Matrix class
+        self.kin = updateKin()  # Kinematics class
 
         """System"""
         self.n = 6  # number of states
         self.A = np.concatenate((np.concatenate((np.identity(self.n), np.zeros((self.n, self.n))), axis=1),
                                  np.concatenate((ts*np.identity(self.n), np.identity(self.n)), axis=1)), axis=0)  # Dynamic Matrix
         self.B = np.zeros((6, 36), dtype=np.float64)  # input Matrix
-        #  self.B = np.zeros((6, 18), dtype=np.float64) # input Matrix
         self.C = np.concatenate((np.identity(self.n), np.zeros((self.n, self.n))), axis=1)  # Exit Matrix
         self.x = np.zeros((1, 12), dtype=np.float)  # Augmented states vector: x = [h[k/k];e[k/k]]
-        self.cmm = CentroidalMomentumMatrix()  # Initialization of the Centroidal Momentum Matrix
         self.cmm_Mtx = np.zeros((6, 18), dtype=np.float)  # Ag matrix
         self.cmm_d_Mtx = np.zeros((6, 18), dtype=np.float)  # Ag dot matrix
-        self.kin = updateKin()
 
         """Constraints"""
-        self.A_Constraints = ''
-        self.b_Constraints = ''
+        self.A_Constraints = ''  # Matrix of the constraints inequation  Ax<=b
+        self.b_Constraints = ''  # Matrix of the constraints inequation  Ax<=b
         self.C1 = ''
-        self.C2 = np.array([[1, 0, -fric_coef],
-                            [-1, 0, -fric_coef],
-                            [0, 1, -fric_coef],
-                            [0, -1, -fric_coef]])
+        self.C2 = ''
         self.C2blk = ''
         self.Wg = ''
         self.CF = ''
-
-        for i in range(self.N):   # Acceleration constraint matrix
-            if i == 0:
-                self.Ca = self.evalca()
-            else:
-                self.Ca = block_diag(self.Ca, self.evalca())
-
-        for i in range(self.N):  # Speed constraint matrix
-            if i == 0:
-                self.Cs = self.evalcs()
-            else:
-                self.Cs = block_diag(self.Cs, self.evalcs())
-
+        self.Ca = ''
+        self.Cs = ''
         self.Cp = np.zeros((self.M * 18, self.M * 36), dtype=np.float64)  # Position constraint matrix
-        for i in range(self.M):
-            for j in range(self.M):
-                if i >= j:
-                    M[i * 18:(i + 1) * 18, j * 36:(j + 1) * 36] = self.evalcp()
-
-        self.q_max = q_max
-        self.q_min = q_min
-        self.qd_max = qd_max
-        self.qd_min = qd_min
-        self.qdd_max = qdd_max
-        self.qdd_min = qdd_min
 
         """Controller Variable"""
         self.G = np.zeros((self.N * self.n * 2, self.M * self.n * 2))
@@ -122,6 +100,12 @@ class ParametersMPC:
         for i in range(self.N):
             self.Phi[i * self.n:self.n * (i + 1), :] = self.C.dot(np.linalg.matrix_power(self.A, i+1))
 
+        '''Gain Matrices'''
+        self.Py = ''
+        self.Pu = ''
+        self.updategainmatrices(Q, R)
+
+    def updategainmatrices(self, Q, R):
         for i in range(self.N):  # States weighting matrix
             if i == 0:
                 self.Py = Q
@@ -143,8 +127,8 @@ class ParametersMPC:
         return auxca
 
     """Evaluate the speed constraint matrix block"""
-    def evalca(self):
-        auxca = np.concatenate((np.zeros((6, 36), dtype=np.float64),
+    def evalcs(self):
+        auxcs = np.concatenate((np.zeros((6, 36), dtype=np.float64),
                                 np.concatenate((np.zeros((12, 6), dtype=np.float64),
                                                 np.identity(12, dtype=np.float64),
                                                 np.zeros((12, 6), dtype=np.float64),
@@ -170,6 +154,95 @@ class ParametersMPC:
         self.mtxSCR[2][0] = q[1]
         self.mtxSCR[2][1] = q[0]
 
+    def updateconstraint(self, Mode, q, fric_coef=None):
+        if fric_coef is None:
+            self.C2 = np.array([[1, 0, -DEFAULT_FRIC_COEF],
+                                [-1, 0, -DEFAULT_FRIC_COEF],
+                                [0, 1, -DEFAULT_FRIC_COEF],
+                                [0, -1, -DEFAULT_FRIC_COEF]])
+        else:
+            self.C2 = np.array([[1, 0, -fric_coef],
+                                [-1, 0, -fric_coef],
+                                [0, 1, -fric_coef],
+                                [0, -1, -fric_coef]])
+
+        if Mode is 'StandUp':
+            """Need to update the values"""
+            q_max = np.array([[2*np.pi/9], [3*np.pi/4], [-2.85],
+                              [-0.05], [1.57], [-2.85],
+                              [-0.05], [-1.57], [2.85],
+                              [-0.05], [-1.57], [2.85]])
+            q_min = np.array([[2*np.pi/9], [3*np.pi/4], [-2.85],
+                              [-0.05], [1.57], [-2.85],
+                              [-0.05], [-1.57], [2.85],
+                              [-0.05], [-1.57], [2.85]])
+            qd_max = np.array([[2*np.pi/9], [3*np.pi/4], [-2.85],
+                              [-0.05], [1.57], [-2.85],
+                              [-0.05], [-1.57], [2.85],
+                              [-0.05], [-1.57], [2.85]])
+            qd_min = np.array([[2*np.pi/9], [3*np.pi/4], [-2.85],
+                              [-0.05], [1.57], [-2.85],
+                              [-0.05], [-1.57], [2.85],
+                              [-0.05], [-1.57], [2.85]])
+            qdd_max = np.array([[2*np.pi/9], [3*np.pi/4], [-2.85],
+                              [-0.05], [1.57], [-2.85],
+                              [-0.05], [-1.57], [2.85],
+                              [-0.05], [-1.57], [2.85]])
+            qdd_min = np.array([[2*np.pi/9], [3*np.pi/4], [-2.85],
+                              [-0.05], [1.57], [-2.85],
+                              [-0.05], [-1.57], [2.85],
+                              [-0.05], [-1.57], [2.85]])
+
+            for i in range(self.N):   # Acceleration constraint matrix
+                if i == 0:
+                    self.Ca = self.evalca()
+                else:
+                    self.Ca = block_diag(self.Ca, self.evalca())
+
+            for i in range(self.N):  # Speed constraint matrix
+                if i == 0:
+                    self.Cs = self.evalcs()
+                else:
+                    self.Cs = block_diag(self.Cs, self.evalcs())
+
+            for i in range(self.M):
+                for j in range(self.M):
+                    if i >= j:
+                        self.Cp[i * 18:(i + 1) * 18, j * 36:(j + 1) * 36] = self.evalcp()
+
+            self.C1 = np.concatenate((np.concatenate((self.scr(self.kin.updateHT_LF_foot(q[0:3])),
+                                                      self.scr(self.kin.updateHT_RF_foot(q[3:6])),
+                                                      self.scr(self.kin.updateHT_LH_foot(q[6:9])),
+                                                      self.scr(self.kin.updateHT_RH_foot(q[9:12]))), axis=1),
+                                      np.ones((3, 12), dtype=np.float64)), axis=0)
+            for i in range(self.M):
+                if i == 0:
+                    self.Wg = self.B
+                else:
+                    self.Wg = block_diag(self.Wg, self.B)
+
+            for i in range(4):
+                if i == 0:
+                    self.C2blk = self.C2
+                else:
+                    self.C2blk = block_diag(self.C2blk, self.C2)
+
+            self.CF = (self.C2blk.dot(np.linalg.pinv(self.C1))).dot(self.Wg)
+            self.A_Constraints = np.vstack((self.CF,
+                                            self.Ca,
+                                            -self.Ca,
+                                            self.Cs,
+                                            -self.Cs,
+                                            self.Cp,
+                                            -self.Cp))
+
+            conTypes = [qdd_max, -qdd_min, qd_max, -qd_min, q_max-q[6:18], -q_min+q[6:18]]
+            self.b_Constraints = np.zeros((16, 1), dtype=np.float64)
+
+            for i in conTypes:
+                for j in range(self.M):
+                    self.b_Constraints = np.vstack(self.b_Constraints, i)
+
     def updatemodel(self, q, qd, ref, mode):
         self.cmm_Mtx = self.cmm.updateCCM(q)  # update the mass matrix Ag
         self.x[0:self.n+1] = self.cmm_Mtx.dot(qd)  # update the momentum values
@@ -177,7 +250,6 @@ class ParametersMPC:
 
         self.cmm_d_Mtx = self.cmm.updateCMMd(q, qd)  # update Ag dot
         self.B = np.concatenate((self.cmm_d_Mtx, self.cmm_Mtx), axis=1)*self.ts  # update matrix B values
-        #  self.B = self.ts*(self.cmmdMtx * self.ts+self.cmmMtx)  #update matrix B values
 
         for i in range(self.N):  # generate the matrix G
             l = i + 1
@@ -189,38 +261,8 @@ class ParametersMPC:
         """H = 2*(G'*Py*G+Pu)"""
         self.H = 2*((np.transpose(self.G).dot(self.Py)).dot(self.G) + self.Pu)
         """F = 2*(Phi*x-Ref)'*Py*G"""
-        self.F = 2 * ((np.transpose(self.Phi.dot(self.x)-ref)).dot(self.Py)).dot(self.G)
+        self.F = 2*((np.transpose(self.Phi.dot(self.x)-ref)).dot(self.Py)).dot(self.G)
 
         """Constraints: the constraints depends from mode of the posture of the robot """
-        self.C1 = np.concatenate((np.concatenate((self.scr(self.kin.updateHT_LF_foot(q[0:3])),
-                                  self.scr(self.kin.updateHT_RF_foot(q[3:6])),
-                                  self.scr(self.kin.updateHT_LH_foot(q[6:9])),
-                                  self.scr(self.kin.updateHT_RH_foot(q[9:12]))), axis=1),
-                                  np.ones((3, 12), dtype=np.float64)), axis=0)
-        for i in range(self.M):
-            if i == 0:
-                self.Wg = self.B
-            else:
-                self.Wg = block_diag(self.Wg, self.B)
-
-        for i in range(4):
-            if i == 0:
-                self.C2blk = self.C2
-            else:
-                self.C2blk = block_diag(self.C2blk, self.C2)
-
-        self.CF = (self.C2blk.dot(np.linalg.pinv(self.C1))).dot(self.Wg)
-        self.A_Constraints = np.vstack((self.CF,
-                                        self.Ca,
-                                        -self.Ca,
-                                        self.Cs,
-                                        -self.Cs,
-                                        self.Cp,
-                                        -self.Cp))
-        self.b_Constraints = np.vstack((np.zeros((16, 1), dtype=np.float64),
-                                        self.qdd_max,
-                                        -self.qdd_min,
-                                        self.qd_max,
-                                        -self.qd_min,
-                                        self.q_max - q[6:18],
-                                        -self.q_min) + q[6:18])
+        mode = 'StandUp'
+        self.updateconstraint(mode, q)
